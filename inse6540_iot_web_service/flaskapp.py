399e191cbeb18f90adcde9b70f418470db0630c7
Brendan Wood <brendan.wood@concordia.ca>
2024-07-25 23:33:35 -0400

    Improving Flask App

--- inse6540_iot_web_service/flaskapp.py
+++ inse6540_iot_web_service/flaskapp.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 def hello():
     return "<b>INSE6540 Project</b>"
 
+
 @app.route("/reading", methods=['POST'])
 def create_reading():
     # reading_value = request.form.get('reading_value')
@@ -16,24 +17,24 @@ def create_reading():
     # reading_timestamp = request.form.get('reading_timestamp')
     # reading_type = request.form.get('reading_type')
 
+    global connection
     incoming = request.json
     reading_value = incoming.get('reading_value')
     reading_value_unit = incoming.get('reading_value_unit')
     reading_timestamp = incoming.get('reading_timestamp')
     reading_type = incoming.get('reading_type')
 
-
     try:
         connection = mysql.connector.connect(host=db_connection_parameters.HOST,
-                                                 database=db_connection_parameters.DATABASE,
-                                                 user=db_connection_parameters.USER,
-                                                 password=db_connection_parameters.PASSWORD)
+                                             database=db_connection_parameters.DATABASE,
+                                             user=db_connection_parameters.USER,
+                                             password=db_connection_parameters.PASSWORD)
 
         mysql_insert_query = (f"INSERT INTO readings "
-                                  f"(date_time, value, unit, reading_type) "
-                                  f"values "
-                                  f"('{reading_timestamp}', '{reading_value}', "
-                                  f"'{reading_value_unit}', '{reading_type}');")
+                              f"(date_time, value, unit, reading_type) "
+                              f"values "
+                              f"('{reading_timestamp}', '{reading_value}', "
+                              f"'{reading_value_unit}', '{reading_type}');")
         print(mysql_insert_query)
 
         cursor = connection.cursor()
