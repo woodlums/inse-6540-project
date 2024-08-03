0c4b230f2b8283712b575da79a4f0b7bbd54bc0c
Brendan Wood <brendan.wood@concordia.ca>
2024-08-03 04:49:24 +0000

    DB connection closing

--- inse6540_iot_web_service/db.py
+++ inse6540_iot_web_service/db.py
@@ -5,32 +5,51 @@ import db_connection_parameters
 class DB:
 
     def __init__(self):
+        self.connect()
+
+    def connect(self):
         self.connection = mysql.connector.connect(host=db_connection_parameters.HOST,
                                                   database=db_connection_parameters.DATABASE,
                                                   user=db_connection_parameters.USER,
                                                   password=db_connection_parameters.PASSWORD)
 
-        self.cursor = self.connection.cursor()
+    def query(self, sql, args):
+        cursor = self.connection.cursor()
+        cursor.execute(sql, args)
+        return cursor
 
     def get_records(self, start_date, end_date):
 
         mysql_query = f"select * from readings_display where CAST(timestamp as DATE) between '{start_date}' and '{end_date}' order by Timestamp desc;"
 
-        self.cursor.execute(mysql_query)
+        self.connect()
 
-        columns = self.cursor.description
-        records = [{columns[index][0].replace(' ', '_'): column for index, column in enumerate(value)} for value in self.cursor.fetchall()]
+        cursor = self.connection.cursor()
+        cursor.execute(mysql_query)
+
+        columns = cursor.description
+        records = [{columns[index][0].replace(' ', '_'): column for index, column in enumerate(value)} for value in cursor.fetchall()]
+
+        cursor.close()
+        self.connection.close()
 
         return records
 
     def get_column_names(self, table_name):
         mysql_query = f"select column_name from INFORMATION_SCHEMA.COLUMNS where table_name = '{table_name}';"
 
-        self.cursor.execute(mysql_query)
+        self.connect()
 
-        columns = self.cursor.description
+        cursor = self.connection.cursor()
+
+        cursor.execute(mysql_query)
+
+        columns = cursor.description
         records = [{columns[index][0].replace(' ', '_'): column for index, column in enumerate(value)} for value in
-                   self.cursor.fetchall()]
+                  cursor.fetchall()]
+
+        cursor.close()
+        self.connection.close()
 
         return [list(x.values())[0] for x in records]
 
@@ -38,20 +57,32 @@ class DB:
 
         mysql_query = f"select * from readings_display order by timestamp desc;"
 
-        self.cursor.execute(mysql_query)
+        cursor = self.connection.cursor()
+        cursor.execute(mysql_query)
 
-        columns = self.cursor.description
-        records = [{columns[index][0].replace(' ', '_'): column for index, column in enumerate(value)} for value in self.cursor.fetchall()]
+        columns = cursor.description
+        records = [{columns[index][0].replace(' ', '_'): column for index, column in enumerate(value)} for value in cursor.fetchall()]
+
+        cursor.close()
 
         return records
 
     def save_reading(self, reading_value, reading_value_unit, reading_timestamp,
                      reading_type, hashed_value):
+
         mysql_insert_query = (f"INSERT INTO readings "
-                              f"(date_time, value, unit, reading_type, hashed_value) "
+                              f"(timestamp, value, unit, reading_type, hashed_value) "
                               f"values "
                               f"('{reading_timestamp}', '{reading_value}', "
                               f"'{reading_value_unit}', '{reading_type}', '{hashed_value}');")
 
-        self.cursor.execute(mysql_insert_query)
+        self.connect();
+        cursor = self.connection.cursor()
+        cursor.execute(mysql_insert_query)
         self.connection.commit()
+        cursor.close()
+        self.connection.close()
+
+#    def __del__(self):
+#        if self.connection != None:
+#            self.connection.close()
